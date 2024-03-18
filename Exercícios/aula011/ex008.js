2b6e3b1382ef964a6aada332b09dffa30fd328c3
Rui Filipe Cardoso <150164913+Ruifilipecardoso@users.noreply.github.com>
2024-03-18 09:35:55 +0000

    Condições

---
+++ Exercícios/aula011/ex008.js
@@ -0,0 +1,10 @@
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {// Condição Simples
+    console.log('Voçê ultrapaçou o limite de velocidade. MULTADO!')
+}
+console.log('Dirija sempre com o cinto de segurança!')
+
+
+
+
---
+++ Exercícios/aula011/ex009.js
@@ -0,0 +1,9 @@
+var país = 'Portugal'
+console.log(`Vivendo em ${país}.`)
+if (país == 'Portugal') {
+    console.log('Voçê é Português')
+} else {
+    console.log('Voçê é Estrangeiro')
+}
+
+
